3d60475c7b4143b8fe71d202ea943c32feeb69b1
= <=>
2024-01-31 20:09:47 +0300

    Added the pick up and drop system, plus some improvements

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody3D
 
 @onready var head: Node3D = $Head
+@onready var cam: Camera3D = $Head/Camera3D
+@onready var pickup_pos: Node3D = $Head/Camera3D/PickupPos
 
+# movement
 const WALKING_SPEED: float   = 5.0
 const SPRINTING_SPEED: float = 8.0
 const CROUCHING_SPEED: float = 3.0
 const JUMP_VELOCITY: float = 4.5
+# pickup and drop
+const PICKUP_LENGTH: float = 2
 
 var mouse_sensitivity: float = 0.4 # TODO: make this a setting so players can change it.
 
@@ -17,21 +22,42 @@ var direction: Vector3 = Vector3.ZERO
 
 var crouching_depth: float = -0.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+var cur_item: Item = null
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		var mouse_motion := event as InputEventMouseMotion
 		rotate_y(deg_to_rad(-mouse_motion.relative.x * mouse_sensitivity))
 		head.rotate_x(deg_to_rad(-mouse_motion.relative.y * mouse_sensitivity))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-85), deg_to_rad(85))
 
-func _physics_process(delta: float) -> void:
+	if event.is_action_pressed("primary_action"):
+		if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if event.is_action_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			
+	if event.is_action_pressed("pickup") and not cur_item:
+		var space_state = get_world_3d().direct_space_state
+		var result = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(
+			cam.global_position, cam.global_position-cam.global_transform.basis.z.normalized()*PICKUP_LENGTH))
+		
+		if result and result.collider.is_in_group("Item"):
+			cur_item = result.collider
+			cur_item.freeze = true
+			cur_item.get_node("CollisionShape3D").disabled = true
+			cur_item.reparent(pickup_pos)
+	if event.is_action_pressed("drop") and cur_item:
+		cur_item.get_node("CollisionShape3D").disabled = false
+		cur_item.reparent(get_parent())
+		cur_item.freeze = false
+		cur_item = null
 
+func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("crouch"): # Crouch is dominant over sprinting.
 		current_speed = CROUCHING_SPEED
 		head.position.y = lerp(head.position.y, player_height + crouching_depth, delta*lerp_speed)
@@ -41,6 +67,10 @@ func _physics_process(delta: float) -> void:
 			current_speed = SPRINTING_SPEED
 		else:
 			current_speed = WALKING_SPEED
+			
+	if cur_item:
+		cur_item.global_position = lerp(cur_item.global_position, pickup_pos.global_position, delta*lerp_speed)
+		cur_item.global_rotation = lerp(cur_item.global_rotation, pickup_pos.global_rotation, delta*lerp_speed)
 
 	# Add the gravity.
 	if not is_on_floor():
@@ -59,7 +89,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, current_speed)
-		velocity.z = move_toward(velocity.z, 0, current_speed)
+		velocity.x = 0
+		velocity.z = 0
 
 	move_and_slide()
