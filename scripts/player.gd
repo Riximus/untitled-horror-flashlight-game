f6667651d7083605b7f885674d29dbfe1d7b7872
Riximus <riximuslol@gmail.com>
2024-02-01 00:22:51 +0100

    feat: added head bobbing

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,9 @@ extends CharacterBody3D
 @onready var standing_collision_shape: CollisionShape3D = $StandingCollisionShape
 @onready var crouching_collision_shape: CollisionShape3D = $CrouchingCollisionShape
 @onready var raycast: RayCast3D = $RayCast3D
-@onready var cam: Camera3D = $Head/Camera3D
-@onready var pickup_pos: Node3D = $Head/Camera3D/PickupPos
+@onready var eyes: Node3D = $Head/Eyes
+@onready var cam: Camera3D = $Head/Eyes/Camera3D
+@onready var pickup_pos: Node3D = $Head/Eyes/Camera3D/PickupPos
 
 # Movement constants
 const WALKING_SPEED: float   = 5.0
@@ -23,6 +24,40 @@ var current_speed: float = 5.0
 var lerp_speed: float = 10.0
 var crouching_depth: float = -0.5
 
+# Head bobbing variables
+var head_bobbing_speed: float = 0.0
+var head_bobbing_intensity: float = 0.0
+var head_bobbing_current_intensity: float = 0.0
+var head_bobbing_index: float = 0.0
+var head_bobbing_vector: Vector2 = Vector2.ZERO
+
+# Player state enum
+enum PLAYER_STATE { IDLE, WALKING, SPRINTING, CROUCHING, JUMPING }
+var current_player_state: PLAYER_STATE = PLAYER_STATE.IDLE
+
+var head_bobbing: Dictionary = {
+	PLAYER_STATE.IDLE: {
+	   "speed": 0,
+	   "intensity": 0
+	},
+	PLAYER_STATE.WALKING: {
+	   "speed": 14.0,
+	   "intensity": 0.1
+	},
+	PLAYER_STATE.SPRINTING: {
+	   "speed": 22.0,
+	   "intensity": 0.2
+	},
+	PLAYER_STATE.CROUCHING: {
+	   "speed": 10.0,
+	   "intensity": 0.05
+	},
+	PLAYER_STATE.JUMPING: {
+	   "speed": 0,
+	   "intensity": 0
+	}
+}
+
 # Input variables
 var direction: Vector3 = Vector3.ZERO
 var mouse_sensitivity: float = 0.4 # TODO: make this a setting so players can change it.
@@ -35,7 +70,7 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event: InputEvent) -> void:
-    # Mouse looking logic
+	# Mouse looking logic
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		var mouse_motion := event as InputEventMouseMotion
 		rotate_y(deg_to_rad(-mouse_motion.relative.x * mouse_sensitivity))
@@ -68,39 +103,64 @@ func _input(event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	# Handle movement states
-	# Crouching
-	if Input.is_action_pressed("crouch") and is_on_floor(): # Crouch is dominant over sprinting.
-		current_speed = CROUCHING_SPEED
-		head.position.y = lerp(head.position.y, player_height + crouching_depth, delta*lerp_speed)
-		standing_collision_shape.disabled = true
-		crouching_collision_shape.disabled = false
-	# Standing
-	elif not raycast.is_colliding() and not Input.is_action_pressed("crouch"):
-		standing_collision_shape.disabled = false
-		crouching_collision_shape.disabled = true
-		head.position.y = lerp(head.position.y, player_height, delta*lerp_speed)
-		# Sprinting
-		if Input.is_action_pressed("sprint"):
-			current_speed = SPRINTING_SPEED
-		# Walking
-		else:
-			current_speed = WALKING_SPEED
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+
+	if is_on_floor():
+		# Idle
+		if input_dir == Vector2.ZERO and is_on_floor():
+			current_player_state = PLAYER_STATE.IDLE
+		# Crouching
+		elif Input.is_action_pressed("crouch") and is_on_floor(): # Crouch is dominant over sprinting.
+			current_player_state = PLAYER_STATE.CROUCHING
+			current_speed = CROUCHING_SPEED
+			head.position.y = lerp(head.position.y, player_height + crouching_depth, delta*lerp_speed)
+			standing_collision_shape.disabled = true
+			crouching_collision_shape.disabled = false
+			# Standing
+		elif not raycast.is_colliding() and not Input.is_action_pressed("crouch"):
+			standing_collision_shape.disabled = false
+			crouching_collision_shape.disabled = true
+			head.position.y = lerp(head.position.y, player_height, delta*lerp_speed)
+			# Sprinting
+			if Input.is_action_pressed("sprint"):
+				current_player_state = PLAYER_STATE.SPRINTING
+				current_speed = SPRINTING_SPEED
+				# Walking
+			else:
+				current_player_state = PLAYER_STATE.WALKING
+				current_speed = WALKING_SPEED
 
 	if cur_item:
 		cur_item.global_position = lerp(cur_item.global_position, pickup_pos.global_position, delta*lerp_speed)
 		cur_item.global_rotation = lerp(cur_item.global_rotation, pickup_pos.global_rotation, delta*lerp_speed)
 
+		# Handle head bobbing
+	if is_on_floor() and not current_player_state == PLAYER_STATE.IDLE:
+		head_bobbing_speed = head_bobbing[current_player_state]["speed"]
+		head_bobbing_intensity = head_bobbing[current_player_state]["intensity"]
+		
+		head_bobbing_current_intensity = head_bobbing_intensity
+		head_bobbing_index += head_bobbing_speed*delta
+		
+		head_bobbing_vector.y = sin(head_bobbing_index)
+		head_bobbing_vector.x = sin(head_bobbing_index/2)+0.5
+		
+		eyes.position.y = lerp(eyes.position.y, head_bobbing_vector.y*(head_bobbing_current_intensity/2.0),delta*lerp_speed)
+		eyes.position.x = lerp(eyes.position.x, head_bobbing_vector.x*(head_bobbing_current_intensity),delta*lerp_speed)
+		
+	else: 
+		eyes.position.y = lerp(eyes.position.y, 0.0, delta*lerp_speed)
+		eyes.position.x = lerp(eyes.position.x, 0.0, delta*lerp_speed)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor() and not Input.is_action_pressed("crouch"):
+		current_player_state = PLAYER_STATE.JUMPING
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta*lerp_speed)
 
 	if direction:
