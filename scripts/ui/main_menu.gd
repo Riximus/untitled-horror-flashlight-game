580e2c37590b98d71205d366506b2630d93253c5
Riximus <riximuslol@gmail.com>
2024-02-08 23:27:17 +0100

    feat: added menu nodes (without logic yet)

---
+++ scripts/ui/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _ready() -> void:
+	pass
+	# TODO: add a check to see if a save exists. If not dont show the "Continue" Button
+
+func _on_new_game_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_load_game_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
