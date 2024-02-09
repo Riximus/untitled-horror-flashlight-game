71b09bf82bee67d779ca5ca5879451a7bc74ccb3
Riximus <riximuslol@gmail.com>
2024-02-09 23:34:49 +0100

    feat: continue button now hides and show if save exists

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,13 +1,19 @@
 extends Control
 
-const new_game_start: PackedScene = preload("res://scenes/main.tscn")
+@onready var CONTINUE_BTN: Button = $PanelContainer/CenterContainer/VBoxContainer/Continue
+
+const NEW_GAME_START: PackedScene = preload("res://scenes/main.tscn")
+const SAVE_FILE_PATH: String      = SaveLoadGame.SAVE_GAME_PATH
 
 func _ready() -> void:
-	pass
-	# TODO: add a check to see if a save exists. If not dont show the "Continue" Button
+	var HAS_SAVE: bool = FileAccess.file_exists(SAVE_FILE_PATH)
+	if HAS_SAVE:
+		CONTINUE_BTN.show()
+	else:
+		CONTINUE_BTN.hide()
 
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_packed(new_game_start)
+	get_tree().change_scene_to_packed(NEW_GAME_START)
 
 func _on_load_game_pressed() -> void:
 	pass # Replace with function body.
